6dba6e240ae799b192fc9b6c6636646f5b913d94
Billie Gray <billiexgray@gmail.com>
2018-09-12 22:38:37 +1000

    removed failed filter function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
-// console.log ('it works')
-
-
-$('.filters-list a').on('click', function(){
-  var filter = $(this).attr('data-filter')
+$('.filters-list a').on('click', function() {
+  var filter = $(this).attr('data-filter');
   
-console.log(filter)
+console.log(filter);
   
-  $('.product').hide()
-  $(filter).show()
+  $('.bloody').hide();
+  $(filter).show();
   
-  $('.filters-list a').removeClass('active')
-  $(this).addClass('active')
-  
+  $('.filters-list a').removeClass('filter-selected');
+  $(this).addClass('filter-selected');
   
 //   this blocks the link from following the href
-  return false
-})
+  return false;
+});
--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,1 @@
-$(".filters-list a").on("click",function(){var t=$(this).attr("data-filter");return console.log(t),$(".product").hide(),$(t).show(),$(".filters-list a").removeClass("active"),$(this).addClass("active"),!1});
+$(".filters-list a").on("click",function(){var e=$(this).attr("data-filter");return console.log(e),$(".bloody").hide(),$(e).show(),$(".filters-list a").removeClass("filter-selected"),$(this).addClass("filter-selected"),!1});
